70306872f205f6c5e8c4a8bf809800c283258fa4
crazyczy <crazyczy@live.cn>
2016-11-03 18:11:17 +0800

    3.0
    去掉了验证码

--- NJNU_Quick_Check_Results_v3.0_GUI.py
+++ NJNU_Quick_Check_Results_v3.0_GUI.py
@@ -3,9 +3,14 @@
 import wx
 import requests
 import hashlib
-from cStringIO import StringIO
+import StringIO
 from bs4 import BeautifulSoup
 import sys
+from PIL import Image
+from PIL import ImageFilter
+from PIL import ImageEnhance
+import pytesseract
+import os
 
 url = 'http://223.2.10.26/cas/logon.action'
 url2 = 'http://223.2.10.26/cas/genValidateCode'
@@ -26,14 +31,6 @@ header2 = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit\
 /537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36', 'Referer': url4}
 
 s = requests.Session()
-
-
-def getValidateCode():
-    r = s.get(url2, headers=header)
-    image = wx.ImageFromStream(
-        StringIO(r.content), wx.BITMAP_TYPE_ANY).ConvertToBitmap()
-    return image
-
 
 def getStatus(username, pwd, yzm):
     data = {
@@ -97,11 +94,33 @@ def getResults():
 
     return (info, grade)
 
+def Verify():
+    r = s.get(url2, headers=header)
+    buff = StringIO.StringIO(r.content)
+    im = Image.open(buff)
+    im = im.filter(ImageFilter.MedianFilter())
+    enhancer = ImageEnhance.Contrast(im)
+    im = enhancer.enhance(2)
+    im = im.convert('1')
+    vcode = pytesseract.image_to_string(im, config='-psm 3')
+    return vcode
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 class MyFrame(wx.Frame):
 
     def __init__(self, *args, **kwds):
         wx.Frame.__init__(self, None, size=(1200, 600))
+        self.icon = wx.Icon(resource_path('icon.ico'), wx.BITMAP_TYPE_ICO)
+        self.SetIcon(self.icon)  
         self.panel = wx.Panel(self)
         self.label_1 = wx.StaticText(self.panel, wx.ID_ANY, u'学号')
         self.text_ctrl_1 = wx.TextCtrl(
@@ -109,11 +128,6 @@ class MyFrame(wx.Frame):
         self.label_2 = wx.StaticText(self.panel, wx.ID_ANY, u'密码')
         self.text_ctrl_2 = wx.TextCtrl(
             self.panel, wx.ID_ANY, style=wx.TE_PASSWORD | wx.TE_PROCESS_ENTER)
-        self.label_3 = wx.StaticText(self.panel, wx.ID_ANY, u'验证码')
-        self.text_ctrl_3 = wx.TextCtrl(
-            self.panel, wx.ID_ANY, style=wx.TE_PROCESS_ENTER)
-        self.bitmap_button_1 = wx.BitmapButton(
-            self.panel, wx.ID_ANY, getValidateCode())
         self.button_1 = wx.Button(self.panel, wx.ID_ANY, u'重置', size=(70, 30))
         self.button_2 = wx.Button(self.panel, wx.ID_ANY, u'查询', size=(70, 30))
         self.text_ctrl_4 = wx.TextCtrl(
@@ -127,12 +141,10 @@ class MyFrame(wx.Frame):
 
         self.__do_layout()
 
-        self.bitmap_button_1.Bind(wx.EVT_BUTTON, self.OnClicked)
         self.button_1.Bind(wx.EVT_BUTTON, self.OnClear)
         self.button_2.Bind(wx.EVT_BUTTON, self.OnSubmit)
         self.text_ctrl_1.Bind(wx.EVT_TEXT_ENTER, self.OnSubmit)
         self.text_ctrl_2.Bind(wx.EVT_TEXT_ENTER, self.OnSubmit)
-        self.text_ctrl_3.Bind(wx.EVT_TEXT_ENTER, self.OnSubmit)
 
         self.Centre()
 
@@ -144,31 +156,23 @@ class MyFrame(wx.Frame):
         sizer.Add(self.text_ctrl_1, (0, 1), (1, 2), wx.EXPAND | wx.ALL, 5)
         sizer.Add(self.label_2, (1, 0), (1, 1), wx.ALL, 5)
         sizer.Add(self.text_ctrl_2, (1, 1), (1, 2), wx.EXPAND | wx.ALL, 5)
-        sizer.Add(self.label_3, (2, 0), (1, 1), wx.ALL, 5)
-        sizer.Add(self.text_ctrl_3, (2, 1), (1, 1), wx.EXPAND | wx.ALL, 5)
-        sizer.Add(self.bitmap_button_1, (2, 2), (1, 1), wx.EXPAND | wx.ALL, 5)
-        sizer.Add(self.button_1, (3, 1), (1, 1), wx.ALL | wx.ALIGN_RIGHT, 5)
-        sizer.Add(self.button_2, (3, 2), (1, 1), wx.ALL | wx.ALIGN_RIGHT, 5)
-        sizer.Add(self.static_bitmap_1, (0, 3), (4, 1), wx.EXPAND | wx.ALL, 5)
-        sizer.Add(self.text_ctrl_4, (0, 4), (4, 1), wx.EXPAND | wx.ALL, 5)
-        sizer.Add(self.list_ctrl_1, (4, 0), (1, 5), wx.EXPAND | wx.ALL, 5)
+        sizer.Add(self.button_1, (2, 1), (1, 1), wx.ALL | wx.ALIGN_RIGHT, 5)
+        sizer.Add(self.button_2, (2, 2), (1, 1), wx.ALL | wx.ALIGN_RIGHT, 5)
+        sizer.Add(self.static_bitmap_1, (0, 3), (3, 1), wx.EXPAND | wx.ALL, 5)
+        sizer.Add(self.text_ctrl_4, (0, 4), (3, 1), wx.EXPAND | wx.ALL, 5)
+        sizer.Add(self.list_ctrl_1, (3, 0), (1, 5), wx.EXPAND | wx.ALL, 5)
 
-        sizer.AddGrowableRow(4, 1)
+        sizer.AddGrowableRow(3, 1)
         sizer.AddGrowableCol(4, 1)
 
         self.panel.SetSizerAndFit(sizer)
 
-    def OnClicked(self, event):
-        self.bitmap_button_1.SetBitmapLabel(getValidateCode())
-
     def OnClear(self, event):
         self.text_ctrl_1.Clear()
         self.text_ctrl_2.Clear()
-        self.text_ctrl_3.Clear()
         self.text_ctrl_4.Clear()
         self.static_bitmap_1.Hide()
         self.list_ctrl_1.ClearAll()
-        self.bitmap_button_1.SetBitmapLabel(getValidateCode())
 
     def OnSubmit(self, event):
 
@@ -176,8 +180,11 @@ class MyFrame(wx.Frame):
         self.list_ctrl_1.ClearAll()
         username = self.text_ctrl_1.GetValue()
         pwd = self.text_ctrl_2.GetValue()
-        yzm = self.text_ctrl_3.GetValue()
+        yzm = Verify()
         status = getStatus(username, pwd, yzm)
+        while(status['status'] == '401'):
+            yzm = Verify()
+            status = getStatus(username, pwd, yzm)
         self.text_ctrl_4.SetValue(status['message'])
         if status['status'] == '200':
             year = username[2:4]
@@ -185,7 +192,7 @@ class MyFrame(wx.Frame):
             turl = photourl + year + '/' + name
             req = requests.get(turl, headers=header)
             if req.headers.get('content-length') != '1163':
-                image = wx.ImageFromStream(StringIO(req.content), wx.BITMAP_TYPE_ANY)
+                image = wx.ImageFromStream(StringIO.StringIO(req.content), wx.BITMAP_TYPE_ANY)
                 image = image.Scale(105, 147, wx.IMAGE_QUALITY_HIGH)
                 result = wx.BitmapFromImage(image)
                 self.static_bitmap_1.SetBitmap(result)
@@ -215,8 +222,7 @@ class MyFrame(wx.Frame):
             for k in range(0, 12):
                 self.list_ctrl_1.SetColumnWidth(k, wx.LIST_AUTOSIZE_USEHEADER)
 
-        self.bitmap_button_1.SetBitmapLabel(getValidateCode())
-
+ 
 if __name__ == '__main__':
     app = wx.App()
     frame = MyFrame(None)
